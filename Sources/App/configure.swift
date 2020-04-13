fde001ff886c98f25c19e00e1979fcb2aea264eb
Michael Critz <github@michaelcritz.com>
2020-04-13 09:55:48 -0700

    first commit

---
+++ Sources/App/configure.swift
@@ -0,0 +1,16 @@
+import Fluent
+import FluentSQLiteDriver
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+
+    app.migrations.add(CreateTodo())
+
+    // register routes
+    try routes(app)
+}
