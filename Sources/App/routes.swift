8077b708c738cbb5aa49783a1913a1cedb72072d
Michael Critz <github@michaelcritz.com>
2020-04-18 20:04:57 -0700

    naive binary upload stream

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -49,5 +49,43 @@ func routes(_ app: Application) throws {
             return image
         }
     }
-
+    
+    
+    
+//    app.on(.POST, "file", body: .stream, use: imageController.upload)
+    app.on(.POST, "file", body: .stream) { req -> EventLoopFuture<HTTPStatus> in
+        let nbFileIO = NonBlockingFileIO(threadPool: app.threadPool)
+        let filePath = app.directory.workingDirectory + "temp-\(UUID().uuidString).tmp"
+        FileManager().createFile(atPath: filePath, contents: nil, attributes: nil)
+        let fileHandle = nbFileIO.openFile(path: filePath, mode: .write, eventLoop: req.eventLoop)
+        let result = fileHandle.map { fHand in
+            var futureFileIOWriters = [EventLoopFuture<()>]()
+            req.body.drain { someResult -> EventLoopFuture<Void> in
+                switch someResult {
+                case .buffer(let buffy):
+                    let wrote = nbFileIO.write(fileHandle: fHand, buffer: buffy, eventLoop: req.eventLoop)
+                    futureFileIOWriters.append(wrote)
+                    return wrote
+                case .error(let err):
+                    debugPrint("error", err)
+                    do {
+                        try fHand.close()
+                        try FileManager().removeItem(atPath: filePath)
+                        debugPrint(".error close")
+                    } catch {
+                        debugPrint("fail", error)
+                    }
+                    return req.eventLoop.makeFailedFuture(err)
+                case .end:
+                    return req.eventLoop.future().fold(futureFileIOWriters) { _,_ in
+                        try? fHand.close()
+                        return req.eventLoop.makeSucceededFuture(())
+                    }
+                }
+            }
+        }
+        return result.map { _ in
+            return HTTPStatus.ok
+        }
+    }
 }
